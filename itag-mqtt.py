8675cd67e6f43469600382a20410d12495e9c5ac
Adrian Sampson <adrian@radbox.org>
2020-12-20 01:29:15 +0000

    Separate clients per thread

--- itag-mqtt.py
+++ itag-mqtt.py
@@ -4,7 +4,12 @@ import paho.mqtt.client as mqtt
 from threading import Thread
 
 
-def notify(device, client):
+def notify(device, host):
+    # Connect to MQTT broker.
+    client = mqtt.Client('itag')
+    client.connect(host)
+    client.loop_start()
+
     proc = subprocess.Popen(
         ['gatttool', '-b', device, '--char-read', '-a', '0x000c', '--listen'],
         stdout=subprocess.PIPE,
@@ -13,21 +18,21 @@ def notify(device, client):
     button_topic = 'itag/{}/button'.format(device)
     connect_topic = 'itag/{}/connect'.format(device)
 
-    for line in proc.stdout:
-        if b'Characteristic' in line:
-            print(connect_topic)
-            client.publish(connect_topic)
-        elif b'Notification' in line:
-            print(button_topic)
-            client.publish(button_topic)
+    try:
+        for line in proc.stdout:
+            if b'Characteristic' in line:
+                print(connect_topic)
+                client.publish(connect_topic)
+                print('sent')
+            elif b'Notification' in line:
+                print(button_topic)
+                client.publish(button_topic)
+                print('sent')
+    finally:
+        client.loop_stop()
 
 
 def main(host):
-    # Connect to MQTT broker.
-    client = mqtt.Client('itag')
-    client.connect(host)
-    client.loop_start()
-
     # Look for devices.
     proc = subprocess.Popen(
         ['script', '-q', '-c', 'hcitool lescan', '/dev/null'],
@@ -37,17 +42,16 @@ def main(host):
         text=True,
     )
 
+    # Launch a thread for each device we find.
     try:
         for line in proc.stdout:
             addr, name = line.strip().split(None, 1)
             if 'iTAG' in name:
                 print('found', addr)
-                notifier = Thread(target=notify, args=(addr, client))
+                notifier = Thread(target=notify, args=(addr, host))
                 notifier.start()
     except KeyboardInterrupt:
         pass
-    finally:
-        client.loop_stop()
 
 
 if __name__ == '__main__':
